8246e2a03a19b5d278692df919d80a6a4f8cd30c
HamimBhai742 <154683268+HamimBhai742@users.noreply.github.com>
2024-02-19 21:29:15 +0600

    Last Update

--- script/index.js
+++ script/index.js
@@ -5,22 +5,7 @@ let totalPrice = 0;
 for (const busSeat of busSeats) {
     console.log(busSeat);
     busSeat.addEventListener('click', function (event) {
-        // const busSeat=document.getElementsByClassName('bus-seat')
-        // if()
-        // const events = event.type
-        // // console.log(events.concat(events));
-        // console.log(events);
-        // if (events === 4) {
-        //     console.log('uixcf');
-        // }
         if (count > 3) {
-            // const busSeatsd=document.getElementsByClassName('bus-seat')
-            // for(const busSeats of busSeatsd){
-            //     const bus=busSeats.classList
-
-            //     console.log(busSeats.classList);
-            // }
-            // console.log(busSeats);
             alert('You cannot buy any more tickets')
             return 'You cannot buy any more tickets'
 
@@ -61,9 +46,7 @@ for (const busSeat of busSeats) {
         buyTicketListclass.add('flex')
         buyTicketListclass.add('justify-between')
 
-        // setTicketPrice('buy-ticket-list')
-        // const totalPrices=document.getElementById('total-price')
-        // console.log(p3);
+
         const perTicket = p3.innerText
         const perTicketPrice = parseInt(perTicket)
         totalPrice += perTicketPrice;
@@ -76,30 +59,7 @@ for (const busSeat of busSeats) {
         const leftSeatNumber = parseInt(leftseatNum)
         const totalLeftSeat = leftSeatNumber - 1;
         leftSeat.innerText = totalLeftSeat;
-        console.log(totalLeftSeat);
-
-        // buyTicekt.innerText=busSeatName;
-        // const p=document.createElement('p')
-        // p.innerText=busSeatName
-        // buyTicekt.appendChild(p)
-        // console.log(count);
-        // const countTicket=document.getElementById('count-buy-ticket')
-        // countTicket.innerText=count;
-        // console.log(busseatList);
-        // const leftSeat = document.getElementById('left-seat').innerText;
-        // const leftSeatNumber = parseInt(leftSeat)
-        // console.log(leftSeatNumber);
-
-        // const event1 = event.type;
-        // if (event1 === 'click') {
-        //     const leftSeat = document.getElementById('left-seat').innerText;
-        //     const leftSeatNumber = parseInt(leftSeat)
-        //     const totalLeftseat = leftSeatNumber - 1;
-        //     console.log(totalLeftseat);
-
-        // }
-        // setLeftSeats('left-seat',65)
-
+        // console.log(totalLeftSeat);
     })
 }
 
@@ -128,28 +88,22 @@ function applyBtn() {
         setGrandPrice('grand-total')
         addClassById('input-cupon')
         addClassById('enabled-btn')
-        console.log(grandTotalPrice);
+        // console.log(grandTotalPrice);
     }
     else if (cuponEvent === 'couple20') {
         const discountPrice = totalPrice * 0.20;
         grandTotalPrice = totalPrice - discountPrice;
-        console.log(grandTotalPrice);
+        // console.log(grandTotalPrice);
         setGrandPrice('grand-total')
         addClassById('input-cupon')
         addClassById('enabled-btn')
     }
     else {
-        alert('Invalid Coupon Code')
+        alert('Invalid Cupon Code')
     }
 
 }
 
-
-// function addEventById(elemntId) {
-//     const addEvents = document.getElementById(elemntId)
-//     const addEvent=addEvents.value.split('').length
-
-// }
 
 document.addEventListener('keyup', function () {
     const passenger1 = document.getElementById('passenger-info')
@@ -182,18 +136,3 @@ continueModal.addEventListener('click',function(){
     const modals=document.getElementById('modal')
     modals.classList.add('hidden')
 })
-// document.getElementById('passenger-info').addEventListener('keyup',function(){
-//     const passenger=document.getElementById('passenger-info')
-//     const passengerFroms=passenger.value.split('').length
-//     if(passengerFroms>3){
-//         enableBtn('enabled-btn-2')
-//     }
-// })
-
-// document.getElementById('number-info').addEventListener('keyup',function(){
-//     const passenger=document.getElementById('number-info')
-//     const passengerFroms=passenger.value.split('').length
-//     if(passengerFroms>10){
-//         enableBtn('enabled-btn-2')
-//     }
-// })
--- script/utility.js
+++ script/utility.js
@@ -1,8 +1,4 @@
-// function setLeftSeats(elementId, leftSeat) {
-//     const setaLeft = document.getElementById(elementId)
-//     setaLeft.innerText=leftSeat;
-//     console.log(setaLeft);
-// }
+
 function countTicket(elementId) {
     const ticketCount = document.getElementById(elementId)
     ticketCount.innerText = count;
@@ -30,20 +26,4 @@ function addClassById(elementId) {
     const element2 = element1.classList
     element2.add('hidden')
     console.log(element2);
-}
-
-// function inputFrom(elementId){
-//     const inputFrom=document.getElementById(elementId)
-//     const inputFromCheak=inputFrom.target.value
-//     console.log(inputFromCheak);
-// }
-function buyTicektList(elementId) {
-    // const buyTicekt=document.getElementById(elementId)
-    // const p=document.createElement('p')
-    // p.innerText=busSeatName
-    // buyTicekt.appendChild(p)
-    // // p.innerText=busSeatName;
-    // // buyTicekt.appendChild(p)
-    // // console.log(buyTicekt);
-    // console.log(buyTicekt);
 }
